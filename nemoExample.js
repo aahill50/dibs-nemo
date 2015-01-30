28d4af7dabdb207f638bca11b7b98ec37d687299
Aaron Hill <aahill50@gmail.com>
2015-01-30 11:43:23 -0500

    Slight refactor

--- nemoExample.js
+++ nemoExample.js
@@ -45,42 +45,32 @@ describe('1stdibs - Global Search', function() {
     it('should show search results when a valid search term is entered', function(done) {
         var page = nemo.view.homepage;
 
-        page.searchBarVisible().then(function(isVisible) {
-            if (!isVisible) {
-                return;
-            }
-            page.searchBar().sendKeys("gold");
-            nemo.driver.sleep(500).then(function() {
-                page.searchResultsVisible().then(function(isVisible) {
-                    if (isVisible) {
-                        return done();
-                    }
-                    return done(new Error("Search results not visible"));
-                }, function(err) {
-                    done(err);
-                });
-            })
+        page.searchBar().sendKeys("gold");
+        nemo.driver.sleep(500).then(function() {
+            page.searchResultsVisible().then(function(isVisible) {
+                if (isVisible) {
+                    return done();
+                }
+                return done(new Error("Search results not visible"));
+            }, function(err) {
+                done(err);
+            });
         })
     });
 
     it('should not show search results when an invalid search term is entered', function(done) {
         var page = nemo.view.homepage;
 
-        page.searchBarVisible().then(function(isVisible) {
-            if (!isVisible) {
-                return;
-            }
-            page.searchBar().sendKeys("hjkfsahjk");
-            nemo.driver.sleep(500).then(function() {
-                page.searchResultsVisible().then(function(isVisible) {
-                    if (!isVisible) {
-                        return done();
-                    }
-                    return done(new Error("Search results were visible"));
-                }, function(err) {
-                    done(err);
-                });
-            })
+        page.searchBar().sendKeys("hjkfsahjk");
+        nemo.driver.sleep(500).then(function() {
+            page.searchResultsVisible().then(function(isVisible) {
+                if (!isVisible) {
+                    return done();
+                }
+                return done(new Error("Search results were visible"));
+            }, function(err) {
+                done(err);
+            });
         })
     });
 });
